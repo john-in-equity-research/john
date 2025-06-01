f8f05b5c074cdf8a6b285377e9872d4445c081ef
john-in-equity-research <mika2003.cc@gmail.com>
2025-06-01 21:50:29 +0200

    Update FMP.py

--- Code/EARN/FMP.py
+++ Code/EARN/FMP.py
@@ -1,8 +1,8 @@
 import requests
-from .speaker import identify_speaker
+from .NER import *
+from .speaker import *
 
-
-def clean_earnings_calls_for_year(ticker, year, key):
+def clean_earnings_calls_for_year(ticker, year, known_speakers, key):
     url = f"https://financialmodelingprep.com/api/v4/batch_earning_call_transcript/{ticker}?year={year}&apikey={key}"
 
     response = requests.get(url)
@@ -14,26 +14,59 @@ def clean_earnings_calls_for_year(ticker, year, key):
         return False
 
     return [
-        clean_earnings_call(d, year=year, ticker=ticker) for d in data
+        clean_earnings_call(d, year=year, ticker=ticker, known_speakers=known_speakers) for d in data
     ]
+
+
+def NER_makes_sense(id_speaker: dict, known_speakers: list):
+    """
+
+     known_speakers: List of known C-levels (e.g. CEO;CFO;VP of IR;Other Exec. e.g. CTO)
+     id_speaker: {full,last,first name}
+     
+     we must rethink this func here tbh.
+
+    """
+
+    return id_speaker["full_name"].upper() == "OPERATOR"
 
 
 def clean_earnings_call(data, **kwargs):
     date = data["date"]
+    known_speakers = kwargs.get("known_speakers")
     quarter = data["quarter"]
-    transcript = data["content"]
-
+    transcript = data["content"].split("\n")
     transcript_cleaned = []
 
-    for i, c in enumerate(transcript.split("\n")):
-        split_two = c.split(":")
-        speaker = split_two[0]
-        merged = "".join(split_two[1:])
+    identified_entities_speaker_org = {}
+
+    org_default = kwargs["ticker"]
+
+    for i, c in enumerate(transcript):
+
+        chunk = c.split(":")
+        speaker = chunk[0]
+        passages = "".join(chunk[1:])
+        id_speaker = identify_speaker(speaker)
+
+        # used to identify analysts and non other non C-levels
+        prev_chunk = transcript[i - 1].split(":")
+        prev_speaker = prev_chunk[0]
+        prev_passages = "".join(prev_chunk[1:])
+        prev_id_speaker = identify_speaker(prev_speaker)
+
+        # see if the prev speaker introduced the current person
+        if NER_makes_sense(prev_id_speaker, known_speakers):
+            identified_entities_speaker_org[id_speaker["full_name"]] = identified_entities_speaker_org.get(
+                id_speaker["full_name"],
+                identify_person_company_relation(prev_passages)
+            )
 
         transcript_cleaned.append(
             {
-                "speaker": identify_speaker(speaker),
-                "text": merged.strip(" ")
+                "text": passages.replace("’", "'").strip(" "),
+                "person": id_speaker,
+                "orgs": identified_entities_speaker_org.get(id_speaker["full_name"], org_default),
             }
         )
 
@@ -45,4 +78,3 @@ def clean_earnings_call(data, **kwargs):
         "frame": f"{kwargs['year']}Q{quarter}",
         "transcript": transcript_cleaned
     }
-    
